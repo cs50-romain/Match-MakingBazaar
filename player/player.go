82fa2f6aaf57017da64128d66b60e76a43f09e23
cs50-romain <cs50.git.romain@gmail.com>
2023-12-12 23:44:49 -0500

    Added Wins property and PrintPlayerStat function

--- player/player.go
+++ player/player.go
@@ -1,17 +1,28 @@
 package player
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type Player struct {
 	Username	string
 	Skill_lvl	int
+	Wins		int
 }
 
 func CreatePlayer(username string) *Player {
 	return &Player{
 		Username: username,
 		Skill_lvl: generateRandomSkillLevel(),
+		Wins: 0,
 	} // Pick a random number for skill level
 }
 
 func generateRandomSkillLevel() int {
-	return 1500
-}	
+	return rand.Intn(100 - 1) + 1
+}
+
+func PrintPlayerStat(player *Player) {
+	fmt.Printf("Username:%s -> |skill level: %d| |Wins:%d|\n", player.Username, player.Skill_lvl, player.Wins)
+}
